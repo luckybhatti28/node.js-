e849b00d14355cbded6ce50df7c99a823021213f
luckybhatti28 <luckybhatti271@gmail.com>
2022-04-25 04:35:34 +0400

    starting node.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// const fs = require('fs');
+
+// fs.writeFileSync("read.txt", "welcome to my channel");
+
+// fs.writeFileSync('read.txt', "Lucky bhatti28 , welcome to my channel");
+
+// fs.appendFileSync('read.txt', " how to you you fine ");
+
+// const buf_data = fs.readFileSync('read.txt')
+
+// console.log(buf_data);
+
+// This show will happen after console ...
+
+// <Buffer 4c 75 63 6b 79 20 62 68 61 74 74 69 32 38 20 2c 20 77 65 6c 63 6f 6d 65 20 74 6f 20 6d 79 20 63 68 61 6e 6e 65 6c 20 0d 0a 68 6f 77 20 74 6f 20 79 6f ... 34 more bytes> 
+
+//Node.js includes an additional data type called buffer ,
+// (not avavilable in browser's Javascript) ,
+// Buffer is mainly used to store binary data ,
+// while reading from a file or receving packets over the networks.
+
+// const orgData = buf_data.toString();
+// console.log(orgData);
+
+// to rename the file ...
+
+// fs.renameSync('read.txt' , "readrename.txt");
+
+
+
+
+
