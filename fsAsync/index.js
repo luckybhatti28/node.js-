e28cd3b63500e1ef63737ce4d6bf7ab2a57deed1
luckybhatti28 <luckybhatti271@gmail.com>
2022-04-27 02:26:37 +0400

    Async and dtf-8

--- fsAsync/index.js
+++ fsAsync/index.js
@@ -6,10 +6,19 @@ const fs = require("fs");
 // });
 
 
+// we pass them a function as an argument -a callback-
+// that gets called when that task completes.
+// the callback has an argument that tells you whether
+// the operation completed successfully.
+// now we need to say what to do when fs.writeFile
+// has complted (even if it's nothing), start
+// checking for errors.
+
+
 //fs.appendFile('read.txt' , 'plz follow my github account', (err)=>{
 //  console.log("task completed");
 //})
 
-fs.readFile('read.txt', "utf-8", (err, data) => {
-    console.log(data);
-})
+// fs.readFile('read.txt', "utf-8", (err, data) => {
+//     console.log(data);
+// })
