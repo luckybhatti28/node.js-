2d3aabd39dceaf52afe4bb28da272bc8a4b0fc8b
luckybhatti28 <luckybhatti271@gmail.com>
2022-04-27 01:05:53 +0400

    Second file updated

---
+++ Secondfiles/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+// fs.mkdirSync('Lucky');
+
+// fs.writeFileSync('Lucky/bio.txt' , "my name is lucky");
+
+// This function is running as it is written in the bio file
+// my name is lucky
+
+// fs.appendFileSync('Lucky/bio.txt' , "i'm Mern stack developer" );
+
+// This function is running as it is written in the bio file
+// i'm Mern stack developer
+
+// const data = fs.readFileSync('Lucky/bio.txt' ,"utf8");
+// console.log(data);
+
+// fs.renameSync('Lucky/bio.txt' , 'Lucky/Mybio.txt')
+
+// fs.unlinkSync('Lucky/Mybio.txt')
+// This function was killed As it is, this file will be deleted
+
+
+
